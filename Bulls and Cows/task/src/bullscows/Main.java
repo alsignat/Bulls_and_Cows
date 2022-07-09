c9508b2838d5e4d07ec853b1a7c87da69658c163
alsignat <alsignat@gmail.com>
2022-07-09 15:49:28 +0300

    stage 3-5 finished

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,23 @@
+package bullscows;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+
+    public static void showExample() {
+        System.out.println("The secret code is prepared: ****.\n" +
+                "\n" +
+                "Turn 1. Answer:\n" +
+                "1234\n" +
+                "Grade: None.\n" +
+                "\n" +
+                "Turn 2. Answer:\n" +
+                "9876\n" +
+                "Grade: 4 bulls.\n" +
+                "Congrats! The secret code is 9876.");
+    }
+}
