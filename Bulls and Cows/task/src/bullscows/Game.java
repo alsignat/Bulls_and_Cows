c015162d767f03c679a6245ceef7a5a8dfa64394
alsignat <alsignat@gmail.com>
2022-07-10 22:10:54 +0300

    stage 7 finished

--- Bulls and Cows/task/src/bullscows/Game.java
+++ Bulls and Cows/task/src/bullscows/Game.java
@@ -10,9 +10,11 @@ public class Game {
     public void play() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter the secret code's length:");
-        int codeLength = scanner.nextInt();
+        int codeLength = toInt(scanner.next());
         System.out.println("Input the number of possible symbols in the code:");
-        int codeSymbols = scanner.nextInt();
+        int codeSymbols = toInt(scanner.next());
+        if (codeLength < 0 || codeSymbols < 0) return;
+        if (!verifyInput(codeLength, codeSymbols)) return;
         Code code = new Code(codeLength, codeSymbols);
         code.printCode(codeSymbols);
         System.out.println("Okay, let's start a game!");
@@ -24,5 +26,25 @@ public class Game {
             finished = code.evaluate(guess);
         }
         System.out.println("Congratulations! You guessed the secret code.");
+    }
+
+    private boolean verifyInput(int length, int symbols) {
+        boolean verified = false;
+        if (symbols > 36) {
+            System.out.println("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).");
+        } else if (symbols < length || length == 0) {
+            System.out.printf("Error: it's not possible to generate a code with a length of %d with %d unique symbols.", length, symbols);
+        } else verified = true;
+        return verified;
+    }
+
+    private int toInt(String input) {
+        int output = -1;
+        try {
+            output = Integer.parseInt(input);
+        } catch (Exception e) {
+            System.out.printf("Error: %s isn't a valid number.%n", input);
+        }
+        return output;
     }
 }
